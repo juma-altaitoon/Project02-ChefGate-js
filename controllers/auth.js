41a97f588db4f0adfce6208467eb3b5b4dcf1255
juma-a <juma.altaitoon@gmail.com>
2022-12-15 13:24:21 +0300

    Version-1.5 Juma - updated auth controllers & routes files. Created signup.ejs file

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,18 @@
+// Require User
+const User = require("../models/User")
+// Require Passport
+const passport = require('../helper/config');
+//  Require bcrypt
+const bcrypt = require("bcrypt");
+const salt = 10;
+
 exports.auth_get = (req, res) => {
     res.send("working on auth file")
+}
+
+exports.auth_signup_get = (req, res) => {
+    res.render("auth/signup");
+}
+exports.auth_login_get = (req,res) => {
+    res.render("auth/login");
 }
