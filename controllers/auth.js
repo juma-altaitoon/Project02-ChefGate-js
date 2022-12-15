b846f25b0139c0ea40487209f40362c3adc2b69c
juma-a <juma.altaitoon@gmail.com>
2022-12-15 16:36:43 +0300

    Update - auth.js in controllers & routes, server.js and updated login.ejs and signup ejs. I also added verify password method to models/User

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,10 +9,45 @@ const salt = 10;
 exports.auth_get = (req, res) => {
     res.send("working on auth file")
 }
-
+// SignUp - GET
 exports.auth_signup_get = (req, res) => {
     res.render("auth/signup");
 }
+// SignUp - POST 
+exports.auth_signup_post = (req,res) => {
+    let user = new User(req,res);
+
+    let hash = bcrypt.hashSync(req.body.password, salt);
+    console.log(hash)// To Check Hash password in database matches
+    user.password = hash;
+
+    // Save user to database
+    user.save()
+    .then (() => {
+        res.redirect('auth/login');
+    })
+    .catch(err => {
+        console.log(err);
+        res.send("Error...Try again later");
+    })
+}
+//  LogIn - GET
 exports.auth_login_get = (req,res) => {
     res.render("auth/login");
 }
+
+//  LogIn - POST
+exports.auth_login_post = passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/auth/login'
+})
+
+// Logout - GET
+// exports.auth_logout_get = (req, res) => {
+//     req.logout(function(err){
+//         if(err){
+//              return next(err);
+//             }
+//         res.redirect('/auth/login');
+//     })
+// }
