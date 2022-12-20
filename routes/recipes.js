5b7f64f067e9f08fa51175d71af0153f5f9d190c
santamew <maytham.aloraibi@gmail.com>
2022-12-20 12:14:12 +0300

    update v 2.2

--- routes/recipes.js
+++ routes/recipes.js
@@ -6,11 +6,16 @@ const router = express.Router();
 const recipeCtrl = require("../controllers/recipes");
 router.get("/recipe", recipeCtrl.recipes_get);
 
-router.get('/recipe/add', recipeCtrl.recipes_create_get);
-// router.post('/recipe/add', recipeCtrl.)
+//GET
+router.get("/recipe/add", recipeCtrl.recipes_create_get);
 router.get("/recipe/details", recipeCtrl.recipes_details_get);
 router.get("recipe/edit", recipeCtrl.recipes_edit_get);
 router.get("/recipe/index", recipeCtrl.recipes_index_get);
 
+//POST
+router.post("/recipe/add", recipeCtrl.recipes_create_post);
+router.post("/recipe/details", recipeCtrl.recipes_details_post);
+router.post("recipe/edit", recipeCtrl.recipes_edit_post);
+router.post("/recipe/index", recipeCtrl.recipes_index_post);
 
 module.exports = router;
